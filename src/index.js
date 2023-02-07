fa81acb9f4c995bf8f6a56ae8627ef6d56603de7
PotapenkoVadim <potapenko.v00@mail.ru>
2023-02-07 18:12:15 +0300

    feat: implement introduce section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
 import './index.html';
 import './styles/global.css';
 
@@ -7,4 +10,24 @@ const header = document.querySelector('.header');
 mobileMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('header__menu_changed');
   header.classList.toggle('header_mobile-active')
+});
+
+
+new Swiper('#cardsSwiper', {
+   slidesPerView: 4,
+   spaceBetween: 40,
+   breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+    780: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+     }
+   }
 });
