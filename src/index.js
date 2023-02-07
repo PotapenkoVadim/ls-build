8b712a48dcae30cc56cec04f44b0e40845a51e6e
PotapenkoVadim <potapenko.v00@mail.ru>
2023-02-07 16:08:51 +0300

    feat: implement header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import './index.html';
 import './styles/global.css';
 
-console.log('Hello')
+const mobileMenu = document.querySelector('.header__menu');
+const header = document.querySelector('.header');
+
+mobileMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('header__menu_changed');
+  header.classList.toggle('header_mobile-active')
+});
